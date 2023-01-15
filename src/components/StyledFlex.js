050c937caeb206d0925f6d585b50a2026e987521
isselin28 <isselinm28@gmail.com>
2023-01-15 20:49:55 +0700

    create component: styledFlex

---
+++ src/components/StyledFlex.js
@@ -0,0 +1,20 @@
+import { Flex } from "@chakra-ui/react";
+
+function StyledFlex(props) {
+  const { children, header, footer } = props;
+
+  let height;
+  if (header || footer) {
+    height = "10%";
+  } else {
+    height = "80%";
+  }
+
+  return (
+    <Flex p={4} backgroundColor="#F4F6F6" h={height} {...props}>
+      {children}
+    </Flex>
+  );
+}
+
+export default StyledFlex;

11074f7be039bcf28701b9ebe2f09557aee82ef4
isselin28 <isselinm28@gmail.com>
2023-01-17 20:38:19 +0700

    display image in main page

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,4 +1,12 @@
-import { Card, CardBody, Box, Text, Heading } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Box,
+  Text,
+  Heading,
+  Image,
+  Flex,
+} from "@chakra-ui/react";
 import { useNavigate } from "react-router";
 
 function ItemCard(props) {
@@ -10,14 +18,23 @@ function ItemCard(props) {
       {menu.map((item, idx) => (
         <Card my={1} w="100%" key={idx}>
           <CardBody>
-            <Box onClick={() => navigate(`/add/${item._id}`)}>
-              <Heading size="sm" textTransform="uppercase">
-                {item.name}
-              </Heading>
-              <Text pt="2" fontSize="sm">
-                Rp. {item.price} / {item.unit}
-              </Text>
-            </Box>
+            <Flex align="center" gap={4}>
+              <Image
+                src={item.image}
+                alt={item.name}
+                borderRadius="full"
+                boxSize="64px"
+                fit="cover"
+              />
+              <Box onClick={() => navigate(`/add/${item._id}`)}>
+                <Heading size="sm" textTransform="uppercase">
+                  {item.name}
+                </Heading>
+                <Text pt="2" fontSize="sm">
+                  Rp. {item.price} / {item.unit}
+                </Text>
+              </Box>
+            </Flex>
           </CardBody>
         </Card>
       ))}

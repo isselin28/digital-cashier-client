f4a3261b98849d2f7b223ebd1d69c5c2cd54761f
isselin28 <isselinm28@gmail.com>
2023-01-19 00:03:02 +0700

    [PageContainer] fix viewport height and width

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -1,13 +1,11 @@
-import { Center, Flex, Box } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 function PageContainer(props) {
   const { children } = props;
   return (
-    <Center w="100%">
-      <Flex m={0} direction="column" h="100vh" minW="400px">
-        <Box flex="1">{children}</Box>
-      </Flex>
-    </Center>
+    <Box w="100%" h="100vh" minW="400px" m={0}>
+      <Box>{children}</Box>
+    </Box>
   );
 }
 

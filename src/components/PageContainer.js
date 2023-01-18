d24cfe1276d9c78ea3e9d2b87e98430c71edbe20
isselin28 <isselinm28@gmail.com>
2023-01-19 00:57:48 +0700

    [PageContainer] use viewport height

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -3,11 +3,9 @@ import { Box } from "@chakra-ui/react";
 function PageContainer(props) {
   const { children } = props;
 
-  const innerHeight = window.innerHeight;
-
   return (
     <Box w="100%" h="100vh" minW="400px" m={0}>
-      <Box h={innerHeight}>{children}</Box>
+      <Box minH="100vh">{children}</Box>
     </Box>
   );
 }

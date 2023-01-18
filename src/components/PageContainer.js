2edb96be72a3964114d0b4b67549da2355701548
isselin28 <isselinm28@gmail.com>
2023-01-19 00:47:19 +0700

    [PageContainer] use innerheight

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -2,9 +2,12 @@ import { Box } from "@chakra-ui/react";
 
 function PageContainer(props) {
   const { children } = props;
+
+  const innerHeight = window.innerHeight;
+
   return (
     <Box w="100%" h="100vh" minW="400px" m={0}>
-      <Box h="100vh">{children}</Box>
+      <Box h={innerHeight}>{children}</Box>
     </Box>
   );
 }

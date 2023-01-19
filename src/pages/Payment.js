58743c4734e1a82843a043c6cffe174685beb2f7
isselin28 <isselinm28@gmail.com>
2023-01-19 16:06:12 +0700

    test print

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -56,7 +56,7 @@ function Payment() {
     getItems();
 
     const formatCashReceived = Number(cashReceived);
-    if (formatCashReceived < total) {
+    if (formatCashReceived === 0) {
       setChange(0);
     } else {
       setChange(formatCashReceived - total);
@@ -74,6 +74,48 @@ function Payment() {
   });
 
   const group = getRootProps();
+
+  function createPrintData() {
+    //Create ESP/POS commands for sample label
+    const esc = "\x1B"; //ESC byte in hex notation
+    const newLine = "\x0A"; //LF byte in hex notation
+
+    let cmds = esc + "@"; //Initializes the printer (ESC @)
+    cmds += esc + "!" + "\x38"; //Emphasized + Double-height + Double-width mode selected (ESC ! (8 + 16 + 32)) 56 dec => 38 hex
+    cmds += "BEST DEAL STORES"; //text to print
+    cmds += newLine + newLine;
+    cmds += esc + "!" + "\x00"; //Character font A selected (ESC ! 0)
+    cmds += "COOKIES                   5.00";
+    cmds += newLine;
+    cmds += "MILK 65 Fl oz             3.78";
+    cmds += newLine + newLine;
+    cmds += "SUBTOTAL                  8.78";
+    cmds += newLine;
+    cmds += "TAX 5%                    0.44";
+    cmds += newLine;
+    cmds += "TOTAL                     9.22";
+    cmds += newLine;
+    cmds += "CASH TEND                10.00";
+    cmds += newLine;
+    cmds += "CASH DUE                  0.78";
+    cmds += newLine + newLine;
+    cmds += esc + "!" + "\x18"; //Emphasized + Double-height mode selected (ESC ! (16 + 8)) 24 dec => 18 hex
+    cmds += "# ITEMS SOLD 2";
+    cmds += esc + "!" + "\x00"; //Character font A selected (ESC ! 0)
+    cmds += newLine + newLine;
+    cmds += "11/03/13  19:53:17";
+
+    console.log(cmds);
+
+    print(cmds);
+  }
+
+  function print(data) {
+    var S = "#Intent;scheme=rawbt;";
+    var P = "package=ru.a402d.rawbtprinter;end;";
+    var textEncoded = encodeURI(data);
+    window.location.href = "intent:" + textEncoded + S + P;
+  }
 
   return (
     <>
@@ -124,7 +166,11 @@ function Payment() {
                 />
               </InputGroup>
               <FormLabel pt="4">Change:</FormLabel>
-              <Heading as="h4" size="md">
+              <Heading
+                as="h4"
+                size="md"
+                color={change < 0 ? "red.600" : "green.400"}
+              >
                 Rp. {change}
               </Heading>
             </CardBody>
@@ -132,8 +178,10 @@ function Payment() {
         )}
       </StyledFlex>
       <StyledFlex footer align="center">
-        <Button colorScheme="green" flex="1" onClick={() => navigate("/cart")}>
+        <Button colorScheme="green" flex="1" onClick={createPrintData}>
+          {/* <a href="intent:data_to_print#Intent;scheme=rawbt;package=ru.a402d.rawbtprinter;end;"> */}
           Print Receipt
+          {/* </a> */}
         </Button>
       </StyledFlex>
     </>

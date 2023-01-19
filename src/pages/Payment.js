fdcb53a1fe730f5dbdd2f49d5aa0c6ad603c2224
isselin28 <isselinm28@gmail.com>
2023-01-19 20:29:44 +0700

    test base64

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -21,17 +21,19 @@ import { NavLink } from "react-router-dom";
 // import { useNavigate } from "react-router";
 import StyledFlex from "../components/StyledFlex";
 import RadioCard from "../components/RadioCard";
+import createPrintData from "../utils/createPrintData";
 
 function Payment() {
-  const [payMethod, setPayMethod] = useState("");
+  const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0);
+  const [payMethod, setPayMethod] = useState("");
   const [cashReceived, setCashReceived] = useState(0);
   const [change, setChange] = useState(0);
   // const navigate = useNavigate();
 
-  // This method fetches the items from the database.
   useEffect(() => {
     const cartState = JSON.parse(localStorage.getItem("cart"));
+    setCart(cartState);
 
     const total = cartState.reduce(
       (total, item) => total + item.price * item.quantity,
@@ -59,48 +61,6 @@ function Payment() {
   });
 
   const group = getRootProps();
-
-  function createPrintData() {
-    //Create ESP/POS commands for sample label
-    const esc = "\x1B"; //ESC byte in hex notation
-    const newLine = "\x0A"; //LF byte in hex notation
-
-    let cmds = esc + "@"; //Initializes the printer (ESC @)
-    cmds += esc + "!" + "\x38"; //Emphasized + Double-height + Double-width mode selected (ESC ! (8 + 16 + 32)) 56 dec => 38 hex
-    cmds += "BEST DEAL STORES"; //text to print
-    cmds += newLine + newLine;
-    cmds += esc + "!" + "\x00"; //Character font A selected (ESC ! 0)
-    cmds += "COOKIES                   5.00";
-    cmds += newLine;
-    cmds += "MILK 65 Fl oz             3.78";
-    cmds += newLine + newLine;
-    cmds += "SUBTOTAL                  8.78";
-    cmds += newLine;
-    cmds += "TAX 5%                    0.44";
-    cmds += newLine;
-    cmds += "TOTAL                     9.22";
-    cmds += newLine;
-    cmds += "CASH TEND                10.00";
-    cmds += newLine;
-    cmds += "CASH DUE                  0.78";
-    cmds += newLine + newLine;
-    cmds += esc + "!" + "\x18"; //Emphasized + Double-height mode selected (ESC ! (16 + 8)) 24 dec => 18 hex
-    cmds += "# ITEMS SOLD 2";
-    cmds += esc + "!" + "\x00"; //Character font A selected (ESC ! 0)
-    cmds += newLine + newLine;
-    cmds += "11/03/13  19:53:17";
-
-    console.log(cmds);
-
-    print(cmds);
-  }
-
-  function print(data) {
-    var S = "#Intent;scheme=rawbt;";
-    var P = "package=ru.a402d.rawbtprinter;end;";
-    var textEncoded = encodeURI(data);
-    window.location.href = "intent:" + textEncoded + S + P;
-  }
 
   return (
     <>
@@ -163,7 +123,11 @@ function Payment() {
         )}
       </StyledFlex>
       <StyledFlex footer align="center">
-        <Button colorScheme="green" flex="1" onClick={createPrintData}>
+        <Button
+          colorScheme="green"
+          flex="1"
+          onClick={() => createPrintData(cart, total)}
+        >
           {/* <a href="intent:data_to_print#Intent;scheme=rawbt;package=ru.a402d.rawbtprinter;end;"> */}
           Print Receipt
           {/* </a> */}

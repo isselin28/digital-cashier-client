5558957de631fb514b98af26c36a91cf4235a9e1
isselin28 <isselinm28@gmail.com>
2023-01-19 21:29:15 +0700

    #1 test print manual

--- src/utils/createPrintData.js
+++ src/utils/createPrintData.js
@@ -2,9 +2,12 @@ import base64data from "./base64.json";
 export default function createPrintData(cart, total) {
   console.log("cart", cart);
   console.log("total", total);
-  //Create ESP/POS commands for sample label
+
+  let result;
+
   const esc = "\x1B"; //ESC byte in hex notation
-  const newLine = "\x0A"; //LF byte in hex notation
+  const newline = "\x0A"; //LF byte in hex notation
+  const space = " ";
 
   const init = esc + "@";
   const title = esc + "!" + "\x38" + "LOTEK KALIPAH APO";
@@ -14,49 +17,54 @@ export default function createPrintData(cart, total) {
   const maxChar = 30;
   const itemList = cart.map((item) => {
     const nameL = item.name.length;
-    const priceL = item.price.length;
-    const totalSpace = maxChar - nameL - priceL;
+    const priceL = String(item.price).length;
+    const spaceL = maxChar - nameL - priceL;
 
-    return `${item.name}                   5.00`;
+    const spacing = space.repeat(spaceL);
+
+    return `${item.name}${spacing}${item.price}` + newline;
   });
+
+  console.log("itemList", itemList);
+
+  result = itemList.join(" ");
+
+  console.log("result", result);
 
   let cmds = esc + "@"; //Initializes the printer (ESC @)
 
   cmds += esc + "!" + "\x38"; //Emphasized + Double-height + Double-width mode selected (ESC ! (8 + 16 + 32)) 56 dec => 38 hex
   cmds += "LOTEK KALIPAH APO"; //text to print
-  cmds += newLine + newLine;
+  cmds += newline + newline;
   cmds += esc + "!" + "\x00"; //Character font A selected (ESC ! 0)
-  cmds += "COOKIES                   5.00";
-  cmds += newLine;
+  cmds += `COOKIES                   5.00`;
+  cmds += newline;
   cmds += "MILK 65 Fl oz             3.78";
-  cmds += newLine + newLine;
+  cmds += newline + newline;
   cmds += "SUBTOTAL                  8.78";
-  cmds += newLine;
+  cmds += newline;
   cmds += "TAX 5%                    0.44";
-  cmds += newLine;
+  cmds += newline;
   cmds += "TOTAL                     9.22";
-  cmds += newLine;
+  cmds += newline;
   cmds += "CASH TEND                10.00";
-  cmds += newLine;
+  cmds += newline;
   cmds += "CASH DUE                  0.78";
-  cmds += newLine + newLine;
+  cmds += newline + newline;
   cmds += esc + "!" + "\x18"; //Emphasized + Double-height mode selected (ESC ! (16 + 8)) 24 dec => 18 hex
   cmds += "# ITEMS SOLD 2";
   cmds += esc + "!" + "\x00"; //Character font A selected (ESC ! 0)
-  cmds += newLine + newLine;
+  cmds += newline + newline;
   cmds += "11/03/13  19:53:17";
 
   console.log(cmds);
 
-  print(cmds);
+  print(result);
 }
-
-console.log("base64data", base64data.data);
 
 function print(data) {
   var S = "#Intent;scheme=rawbt;";
   var P = "package=ru.a402d.rawbtprinter;end;";
-  const base64 = "rawbt:url:base64,aHR0cHM6Ly93d3cuYmFzZTY0ZW5jb2RlLm9yZy8=";
   var textEncoded = encodeURI(data);
-  window.location.href = "intent:" + base64data.data + S + P;
+  window.location.href = "intent:" + textEncoded + S + P;
 }

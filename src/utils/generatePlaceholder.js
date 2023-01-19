5adb666e472044c576c6b98cb33084db141f0c07
isselin28 <isselinm28@gmail.com>
2023-01-19 23:14:19 +0700

    [Placeholder] add colors

--- src/utils/generatePlaceholder.js
+++ src/utils/generatePlaceholder.js
@@ -1,4 +1,12 @@
-const colors = ["F6AD55", "68D391", "4FD1C5", "E9D8FD", "F687B3"];
+const colors = [
+  "F6AD55",
+  "F6E05E",
+  "68D391",
+  "4FD1C5",
+  "76E4F7",
+  "9DECF9",
+  "F687B3",
+];
 const randomColor = colors[Math.floor(Math.random() * colors.length)];
 
 export default function generatePlaceholder(name) {
